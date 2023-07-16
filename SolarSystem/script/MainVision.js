78f5496f36b2a0b75e1da031397ff695b3f3817d
VampireDetached <1478173643@qq.com>
2023-07-16 10:52:26 +0900

    Feat Export MainVision

--- SolarSystem/script/MainVision.js
+++ SolarSystem/script/MainVision.js
@@ -1,30 +1,24 @@
-    class MainVision{
-        constructor(){
+export default class MainVision{
+    constructor(canvas,engine,scene,camera){
+        this.canvas = canvas;
+        this.engine = engine;
+        this.scene = scene;
+        this.camera = camera;
+    }
+    Building(){
             if (!BABYLON.Engine.isSupported()) {
                 window.alert('Browser not supported');
             } else {
                 // 初始化场景要素
-                var canvas = document.getElementById("overallCanvas");                
-                var engine = new BABYLON.Engine(canvas0, true);
-                var scene = new BABYLON.Scene(engine0);
-                var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", -Math.PI / 4.0, 0.25 * Math.PI, 1.5, new BABYLON.Vector3(0, 0, 0), scene0);
-                // Attach the camera to the canvas
-                camera.attachControl(canvas0, true);
-
-
-                this.canvas = canvas;
-                this.engine = engine;
-                this.scene = scene;
-                this.camera = camera;
-
-
-            
                 // 配置场景
-                setUp(engine0, scene0, canvas0, camera0, false, "");
-                setUp(engine1, scene1, canvas1, camera1, true, "sun");
-            
-        }
-        setUp(engine, scene, canvas, camera, setUpParent, parent) {
+                setUp(this.engine, this.scene, this.canvas, this.camera, false, "");
+                setUpCameraMovingPattern(this.camera, this.scene);
+                window.addEventListener("resize", function () {
+                    this.engine.resize();
+                });
+            }
+    }
+    setUp(engine, scene, canvas, camera) {
 
             //diameterScale
             var diameterScale = 35;
@@ -60,11 +54,8 @@
             this.earth = BABYLON.Mesh.CreateSphere("Earth", 0.12756 * diameterScale, 0.3, scene);
             this.moon = BABYLON.Mesh.CreateSphere("Moon", 5, 0.075, scene);
         
-            if (setUpParent === true) {
-                if (parent === "sun") {
-                    camera.parent = sun;
-                }
-            }
+
+            camera.parent = sun;
         
             //Bonusteil:2
             this.mars = BABYLON.Mesh.CreateSphere("Mars", mars_diameter, 0.25, scene);
@@ -144,40 +135,41 @@
             //console.log(sun.position);
         
             // Set initial earth position
-            earth.position.x = earth_distance;
-            earth.position.y = 0.0;
-            earth.position.z = 0.0;
+            this.earth.position.x = this.earth_distance;
+            this.earth.position.y = 0.0;
+            this.earth.position.z = 0.0;
         
             // Set initial moon position
-            moon.position.x = earth.position.x - moon_distance;
-            moon.position.y = earth.position.y;
-            moon.position.z = earth.position.z;
+            this.moon.position.x = this.earth.position.x - this.moon_distance;
+            this.moon.position.y = this.earth.position.y;
+            this.moon.position.z = this.earth.position.z;
         
         
             // Set initial mars position
-            mars.position.x = mars_distance
-            mars.position.y = earth.position.y;
-            mars.position.z = earth.position.z;
+            this.mars.position.x = this.mars_distance
+            this.mars.position.y = this.earth.position.y;
+            this.mars.position.z = this.earth.position.z;
         
         
             // Set initial jupiter position
-            jupiter.position.x = jupiter_distance;
-            jupiter.position.y = earth.position.y;
-            jupiter.position.z = earth.position.z;
+            this.jupiter.position.x = this.jupiter_distance;
+            this.jupiter.position.y = this.earth.position.y;
+            this.jupiter.position.z = this.earth.position.z;
         
             // Set initial neptune position
-            neptune.position.x = neptune_distance;
-            neptune.position.y = earth.position.y;
-            neptune.position.z = earth.position.z;
+            this.neptune.position.x = this.neptune_distance;
+            this.neptune.position.y = this.earth.position.y;
+            this.neptune.position.z = this.earth.position.z;
         
         
-            var earthSpeed = 0;
-            var moonSpeed = 0;
-            var earthOrbitRadius = earth_distance;
-            var moonOrbitRadius = moon_distance;
+            this.earthSpeed = 0;
+            this.moonSpeed = 0;
+            this.earthOrbitRadius = this.earth_distance;
+            this.moonOrbitRadius = this.moon_distance;
             scene.beforeRender = function () {
                 var incremental = false;
                 var incremental_buggy = false;
+
         
                 var d = new Date();
                 var time = d.getTime();        // get milliseconds since 1970
@@ -191,227 +183,127 @@
                 // Update earth position and rotation
                 earthSpeed = ((elapsed_t % einUmlauf) * 360) / einUmlauf;
                 var radians = earthSpeed * Math.PI / 180;
-                earth.position.x = Math.cos(radians) * earthOrbitRadius;
-                earth.position.z = Math.sin(radians) * earthOrbitRadius;
+                this.earth.position.x = Math.cos(radians) * this.earthOrbitRadius;
+                this.earth.position.z = Math.sin(radians) * this.earthOrbitRadius;
         
-                earth.rotation.y = ((elapsed_t) * (360 * 365.24)) / min2ms / 1000;
+                this.earth.rotation.y = ((elapsed_t) * (360 * 365.24)) / min2ms / 1000;
         
         
                 // Update moon position and rotation
                 moonSpeed = ((elapsed_t % (einUmlauf)) * 360) / (27.3 * (einUmlauf / 365.24));
                 var moonradians = moonSpeed * Math.PI / 180;
-                moon.position.x = (Math.cos(moonradians) * moonOrbitRadius) + earth.position.x;
-                moon.position.z = (Math.sin(moonradians) * moonOrbitRadius) + earth.position.z;
+                this.moon.position.x = (Math.cos(moonradians) * this.moonOrbitRadius) + this.earth.position.x;
+                this.moon.position.z = (Math.sin(moonradians) * this.moonOrbitRadius) + this.earth.position.z;
         
-                moon.rotation.y = ((elapsed_t) * (360 * 27.3)) / min2ms;
+                this.moon.rotation.y = ((elapsed_t) * (360 * 27.3)) / min2ms;
         
                 // Update other planets position and rotation
-                mars.position.x = Math.cos(radians * 1.3) * mars_distance;
-                mars.position.z = Math.sin(radians * 1.3) * mars_distance;
+                this.mars.position.x = Math.cos(radians * 1.3) * this.mars_distance;
+                this.mars.position.z = Math.sin(radians * 1.3) * this.mars_distance;
         
-                jupiter.position.x = Math.cos(radians * 1.2) * jupiter_distance;
-                jupiter.position.z = Math.sin(radians * 1.2) * jupiter_distance;
+                this.jupiter.position.x = Math.cos(radians * 1.2) * this.jupiter_distance;
+                this.jupiter.position.z = Math.sin(radians * 1.2) * this.jupiter_distance;
         
-                neptune.position.x = Math.cos(radians * 0.9) * neptune_distance;
-                neptune.position.z = Math.sin(radians * 0.9) * neptune_distance;
+                this.neptune.position.x = Math.cos(radians * 0.9) * this.neptune_distance;
+                this.neptune.position.z = Math.sin(radians * 0.9) * this.neptune_distance;
         
                 //console.log("Earth position: " + moon.position);
                 //console.log(BABYLON.Tools.GetFps().toFixed() + " fps");
             };
-            if (setUpParent === true && parent === "sun") {
-                camera.fov = 0.5;
-                function updateCamera() {
-                    // Calculate the direction from the Earth to the Sun
-                    var direction = BABYLON.Vector3.Normalize(earth.position.subtract(sun.position));
-                    //console.log(`earth.position: ${earth.position}`)
-                    //console.log(`camera: ${camera}`)
-                    // Set the camera's position relative to the Earth (adjust this value based on your desired camera distance)
-                    camera.position = earth.position.add(direction.scale(0.8));
+
+            camera.fov = 0.5;
+            function updateCamera() {
+                // Calculate the direction from the Earth to the Sun
+                var direction = BABYLON.Vector3.Normalize(this.earth.position.subtract(this.sun.position));
+                //console.log(`earth.position: ${earth.position}`)
+                //console.log(`camera: ${camera}`)
+                // Set the camera's position relative to the Earth (adjust this value based on your desired camera distance)
+                camera.position = this.earth.position.add(direction.scale(0.8));
         
-                    // Set the camera's target to always look at the Sun
-                    camera.setTarget(sun.position);
-                }
-        
-                // Register the update function to be called every frame
-                scene.registerBeforeRender(updateCamera);
+                // Set the camera's target to always look at the Sun
+                camera.setTarget(this.sun.position);
             }
+        
+            // Register the update function to be called every frame
+            scene.registerBeforeRender(updateCamera);
+
         
         
             engine.runRenderLoop(function () {
                 scene.render();
             }
             );
-        }
     }
-
-
-
-
-
-
-
-
-
-
-// Check support
-
-    // Kamera für VR mit Device Orientation zum Herumschauen 
-    //var camera = new BABYLON.DeviceOrientationCamera("ArcRotateCamera", new BABYLON.Vector3(1, 1, 1), scene);
-    //camera.setCameraRigMode(20,{interaxialDistance: 0.0637});
-
-
-
-
-    // Show coordinate system, BabylonJS uses left-handed coordinates!
-    // x-axis: red, y-axis: yellow, z-axis: green
-
-
-
-    // Function to update the camera's position and target
-
-
-    //scene.activeCamera.attachControl(canvas);
-    // Render loop
-    // var renderLoop = function () {
-    // Start new frame
-    //engine.beginFrame();
-
-    //scene.render();
-
-    // Present
-    //engine.endFrame();
-
-    // Register new frame
-    //BABYLON.Tools.QueueNewFrame(renderLoop);
-    //};
-    //BABYLON.Tools.QueueNewFrame(renderLoop);
-
-    //canvas1.requestPictureInPicture();
-    // 将canvas转换为video
-    const video = document.getElementById('video');
-
-    // Create a MediaStream object from the canvas
-    const stream = canvas1.captureStream();
-
-    // Create a new MediaRecorder instance for recording video
-    const recorder = new MediaRecorder(stream);
-
-    // Create an array to store the recorded video chunks
-    var chunks = [];
-
-    // Listen for dataavailable event which is triggered when a video chunk is available
-    recorder.addEventListener('dataavailable', (event) => {
-        chunks.push(event.data);
-    });
-
-    // Listen for stop event which is triggered when recording has stopped
-    recorder.addEventListener('stop', () => {
-
-    });
-
-    // Start recording the canvas
-    recorder.start();
-
-    // Stop recording after 5 seconds (adjust this according to your needs)
-    setInterval(() => {
-
-        // Create a Blob object from the recorded chunks
-        const blob = new Blob(chunks, { type: 'video/webm' });
-        recorder.stop();
-        // Set the source of the video element to the recorded video
-        const videoURL = URL.createObjectURL(blob);
-        video.src = videoURL;
-        chunks = [];
-        recorder.start();
-
-    }, 2000);
-
-    //必须要video的metadata加载完成后才能进入画中画
-    video.addEventListener('loadedmetadata', () => {
-        // 在元数据加载完毕后调用 requestPictureInPicture 方法
-        // 必须要有用户动作才能触发画中画，不可以直接用代码开启
-        btn.addEventListener('click', () => {
-            video.requestPictureInPicture()
-                .then(() => {
-                    // 成功进入画中画模式
-                })
-                .catch((error) => {
-                    // 处理错误
-                    console.error('Failed to enter picture-in-picture mode:', error);
-                });
-        });
-    });
-
-    setUpCameraMovingPattern(camera0, scene0);
-
-
-    // Resize
-    window.addEventListener("resize", function () {
-        engine0.resize();
-    });
-};
-
-
-
-
-function setUpCameraMovingPattern(camera, scene) {
-    // Define the movement speed for the camera
-    var cameraSpeed = 0.1;
-
-    // Add event listeners to detect key presses
-    window.addEventListener("keydown", function (event) {
-        var keyCode = event.keyCode;
-
-        // Move forward (W key)
-        if (keyCode === 87) {
-            moveCameraForward();
-        }
-
-        // Move backward (S key)
-        if (keyCode === 83) {
-            moveCameraBackward();
-        }
-
-        // Move left (A key)
-        if (keyCode === 65) {
-            moveCameraLeft();
-        }
-
-        // Move right (D key)
-        if (keyCode === 68) {
-            moveCameraRight();
-        }
-    });
-
-    // Helper functions to move the camera
-    function moveCameraForward() {
-        var direction = BABYLON.Vector3.Normalize(camera.getDirection(BABYLON.Axis.Z));
-        var cur_position = camera.position;
-        camera.position = new BABYLON.Vector3(cur_position.x + cameraSpeed * direction.x,
-            cur_position.y + cameraSpeed * direction.y, cur_position.z + cameraSpeed * direction.z);
-    }
-
-    function moveCameraBackward() {
-        var direction = BABYLON.Vector3.Normalize(camera.getDirection(BABYLON.Axis.Z));
-        var cur_position = camera.position;
-        camera.position = new BABYLON.Vector3(cur_position.x - cameraSpeed * direction.x,
-            cur_position.y - cameraSpeed * direction.y, cur_position.z - cameraSpeed * direction.z);
-    }
-
-    function moveCameraLeft() {
-        var direction = BABYLON.Vector3.Normalize(camera.getDirection(BABYLON.Axis.Z));
-        var sideDirection = BABYLON.Vector3.Cross(direction, camera.upVector);
-        var cur_position = camera.position;
-        camera.position = new BABYLON.Vector3(cur_position.x + cameraSpeed * sideDirection.x,
-            cur_position.y + cameraSpeed * sideDirection.y, cur_position.z + cameraSpeed * sideDirection.z);
-        // camera.position.addInPlace(sideDirection.scaleInPlace(-cameraSpeed));
-    }
-
-    function moveCameraRight() {
-        var direction = BABYLON.Vector3.Normalize(camera.getDirection(BABYLON.Axis.Z));
-        var sideDirection = BABYLON.Vector3.Cross(direction, camera.upVector);
-        var cur_position = camera.position;
-        camera.position = new BABYLON.Vector3(cur_position.x - cameraSpeed * sideDirection.x,
-            cur_position.y - cameraSpeed * sideDirection.y, cur_position.z - cameraSpeed * sideDirection.z);
+    setUpCameraMovingPattern(camera, scene) {
+            // Define the movement speed for the camera
+            var cameraSpeed = 0.1;
+            this.cameraSpeed = cameraSpeed;
+            // Add event listeners to detect key presses
+            window.addEventListener("keydown", function (event) {
+                var keyCode = event.keyCode;
+        
+                // Move forward (W key)
+                if (keyCode === 87) {
+                    moveCameraForward();
+                }
+        
+                // Move backward (S key)
+                if (keyCode === 83) {
+                    moveCameraBackward();
+                }
+        
+                // Move left (A key)
+                if (keyCode === 65) {
+                    moveCameraLeft();
+                }
+        
+                // Move right (D key)
+                if (keyCode === 68) {
+                    moveCameraRight();
+                }
+            });
+        
+            // Helper functions to move the camera
+            function moveCameraForward() {
+                var direction = BABYLON.Vector3.Normalize(camera.getDirection(BABYLON.Axis.Z));
+                var cur_position = camera.position;
+                camera.position = new BABYLON.Vector3(cur_position.x + this.cameraSpeed * direction.x,
+                    cur_position.y + this.cameraSpeed * direction.y, cur_position.z + this.cameraSpeed * direction.z);
+            }
+        
+            function moveCameraBackward() {
+                var direction = BABYLON.Vector3.Normalize(camera.getDirection(BABYLON.Axis.Z));
+                var cur_position = camera.position;
+                camera.position = new BABYLON.Vector3(cur_position.x - this.cameraSpeed * direction.x,
+                    cur_position.y - this.cameraSpeed * direction.y, cur_position.z - this.cameraSpeed * direction.z);
+            }
+        
+            function moveCameraLeft() {
+                var direction = BABYLON.Vector3.Normalize(camera.getDirection(BABYLON.Axis.Z));
+                var sideDirection = BABYLON.Vector3.Cross(direction, camera.upVector);
+                var cur_position = camera.position;
+                camera.position = new BABYLON.Vector3(cur_position.x + this.cameraSpeed * sideDirection.x,
+                    cur_position.y + this.cameraSpeed * sideDirection.y, cur_position.z + this.cameraSpeed * sideDirection.z);
+                // camera.position.addInPlace(sideDirection.scaleInPlace(-cameraSpeed));
+            }
+        
+            function moveCameraRight() {
+                var direction = BABYLON.Vector3.Normalize(camera.getDirection(BABYLON.Axis.Z));
+                var sideDirection = BABYLON.Vector3.Cross(direction, camera.upVector);
+                var cur_position = camera.position;
+                camera.position = new BABYLON.Vector3(cur_position.x - this.cameraSpeed * sideDirection.x,
+                    cur_position.y - this.cameraSpeed * sideDirection.y, cur_position.z - this.cameraSpeed * sideDirection.z);
+            }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
